ae69e282cda03bddac5e03b21c7e994eaf48a342
tpacker <ThomasPacker@gmail.com>
2016-02-20 21:49:11 -0700

    Using MongoDB for key-value pair
    Adding an email address in GlobalData

--- brightdebate/brightdebate.js
+++ brightdebate/brightdebate.js
@@ -1,23 +1,56 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+//  Mongo collections
+GlobalData = new Mongo.Collection("global-data");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+// Initialize DB
+Meteor.startup(function()
+{
+	if (typeof GlobalData.findOne() === 'undefined')
+	{
+		GlobalData.insert({name: 'feedback-email', value: 'ThomasPacker@gmail.com'});
+	}
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
+
+if (Meteor.isClient) 
+{
+	// counter starts at 0
+	Session.setDefault('counter', 0);
+
+	Template.hello.helpers
+	({
+		counter: function () 
+		{
+			return Session.get('counter');
+		}
+	});
+
+	Template.hello.events
+	({
+		'click button': function () 
+		{
+			// increment the counter when button is clicked
+			Session.set('counter', Session.get('counter') + 1);
+		}
+	});
+	
+	
+	Template.footer.helpers
+	({
+		feedbackEmail: function() 
+		{
+			console.log(GlobalData.findOne({name: 'feedback-email'}).value);
+			return GlobalData.findOne({name: 'feedback-email'}).value;
+		}	
+	});
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
+
+if (Meteor.isServer) 
+{
+	Meteor.startup(function () 
+	{
+		// code to run on server at startup
+	});
 }
