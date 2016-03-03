a6e2d47d1b0aeb160acaf75b1aa16b444bdf59f3
tpacker <ThomasPacker@gmail.com>
2016-03-02 21:48:21 -0700

    added link form
    for linkranker

--- brightdebate/brightdebate.js
+++ brightdebate/brightdebate.js
@@ -1,6 +1,12 @@
+//  Global 
+
 //  Mongo collections
 GlobalData = new Mongo.Collection("global-data");
+Tags = new Mongo.Collection("tags");
+Links = new Mongo.Collection("links");
 Propositions = new Mongo.Collection("propositions");
+
+
 
 // Initialize DB
 Meteor.startup(function()
@@ -13,13 +19,14 @@ Meteor.startup(function()
 
 
 
+//  Client side
 if (Meteor.isClient) 
 {
 	// counter starts at 0
 	Session.setDefault('counter', 0);
-
 	
-	Template.hello.helpers
+	
+	Template.clickCounter.helpers
 	({
 		counter: function () 
 		{
@@ -27,7 +34,7 @@ if (Meteor.isClient)
 		}
 	});
 
-	Template.hello.events
+	Template.clickCounter.events
 	({
 		'click button': function () 
 		{
@@ -39,14 +46,61 @@ if (Meteor.isClient)
 	
 	Template.body.helpers
 	({
+		tags: function()
+		{
+			return Tags.find({}, {sort: {createdAt: -1}});
+		},
+		links: function()
+		{
+			return Links.find({}, {sort: {createdAt: -1}});
+		},
 		propositions: function()
 		{
-			return Propositions.find({});
+			return Propositions.find({}, {sort: {createdAt: -1}});
 		}
 	});
 	
 	Template.body.events
 	({
+		"submit .new-tag": function(event)
+		{
+			// Prevent default browser form submit
+			event.preventDefault();
+			
+			// Get value from form element
+			var text = event.target.text.value;
+			
+			// Insert a task into the collection
+			Tags.insert
+			({
+				text: text,
+				createdAt: new Date() // current time
+			});
+			
+			// Clear form
+			event.target.text.value = "";
+		},
+		"submit .new-link": function(event)
+		{
+			// Prevent default browser form submit
+			event.preventDefault();
+			
+			// Get value from form element
+			var title = event.target.title.value;
+			var url = event.target.url.value;
+			
+			// Insert a task into the collection
+			Links.insert
+			({
+				title: title,
+				url: url,
+				createdAt: new Date() // current time
+			});
+			
+			// Clear form
+			event.target.title.value = "";
+			event.target.url.value = "";
+		},
 		"submit .new-proposition": function(event)
 		{
 			// Prevent default browser form submit
@@ -54,7 +108,7 @@ if (Meteor.isClient)
 			
 			// Get value from form element
 			var text = event.target.text.value;
-
+			
 			// Insert a task into the collection
 			Propositions.insert
 			({
@@ -80,6 +134,7 @@ if (Meteor.isClient)
 
 
 
+//  Server
 if (Meteor.isServer) 
 {
 	Meteor.startup(function () 
